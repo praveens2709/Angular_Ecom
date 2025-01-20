bcd92af2226298cdb650e80826276ee6eee6621d
praveens2709 <praveen1892293@gmail.com>
2025-01-20 19:06:41 +0530

    some design fixes

--- src/app/Public/shared/price-details/price-details.component.ts
+++ src/app/Public/shared/price-details/price-details.component.ts
@@ -11,12 +11,18 @@ export class PriceDetailsComponent {
   @Input() priceDetails: any;
   @Input() buttonText: string = '';
   @Input() buttonLink: string = '';
-  @Input() isButtonDisabled: boolean = false; // Control button state
-  @Output() buttonClick: EventEmitter<void> = new EventEmitter<void>(); // Emit custom events for action
+  @Input() isButtonDisabled: boolean = false;
+  @Input() warningMessage: string = 'Please select at least one item to continue';
+
+  @Output() buttonClick: EventEmitter<void> = new EventEmitter<void>();
+
+  showWarning: boolean = false;
 
   onButtonClick(): void {
     if (!this.isButtonDisabled) {
       this.buttonClick.emit();
+    } else {
+      this.showWarning = true;
     }
   }
 }
